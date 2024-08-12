955f8bc4b9f7b4f38a4fc4e0b372bff89421ee23
Shobhit Tomer <shobhittomer0810@gmail.com>
2024-08-12 11:42:05 +0530

    shadcn ui install

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return(
     <>
       <h1>Shobhit tomer is here</h1>
+      <Button variant={"destructive"}>Click me</Button>
     </>
   )
 }
